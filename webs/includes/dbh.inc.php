3e89604c463a75ee7f184a11c425d148e814224a
mariahelenavanatoa <48719355+mariahelenavanatoa@users.noreply.github.com>
2019-04-08 12:25:19 +0300

    Delete dbh.inc.php

--- webs/includes/dbh.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbServername = "mysql.metropolia.fi";
-$dbUsername = "mariamv";
-$dbPassword = "A1793159357a";
-$dbName = "mariamv";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
